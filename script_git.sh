d63b991acb58f42d391843eb9e2f34b16e129eda
amritpalsingh <amritpal.linux@gmail.com>
2019-06-01 15:51:55 +0530

    amrit

--- script_git.sh
+++ script_git.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 echo "Git all at once"
-git add .
-git commit -m $1
-git push origin master
+if [ $# -ne 2 ]
+then 
+	git add .
+	git commit -m $1
+	git push origin master
+else
+	echo "$0 \"commit_sentance\""
+fi
